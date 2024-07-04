07ea8f455757fa59f5f6e1813125d4d64189f02c
Namathieu <164276222+Namathieu@users.noreply.github.com>
2024-07-04 16:15:53 -0400

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,1 +1,38 @@
+class Auth {
+    constructor() {
+        this.users = [
+            { username: 'Ben', password: 'Ben', isAdmin: false },
+            { username: 'Eric', password: 'Eric', isAdmin: false },
+            { username: 'Admin', password: 'Gogi1998!', isAdmin: true }
+        ];
+    }
 
+    login(username, password) {
+        const user = this.users.find(u => u.username === username && u.password === password);
+        if (user) {
+            sessionStorage.setItem('currentUser', JSON.stringify(user));
+            return user;
+        }
+        return null;
+    }
+
+    logout() {
+        sessionStorage.removeItem('currentUser');
+    }
+
+    getCurrentUser() {
+        const userJSON = sessionStorage.getItem('currentUser');
+        return userJSON ? JSON.parse(userJSON) : null;
+    }
+
+    isLoggedIn() {
+        return !!this.getCurrentUser();
+    }
+
+    isAdmin() {
+        const user = this.getCurrentUser();
+        return user && user.isAdmin;
+    }
+}
+
+const auth = new Auth();
